f6efdc0ee18af917aec70cb7f4c4c3c23c428c28
erincav <cavanagherinm@gmail.com>
2017-04-01 04:51:26 -0400

    Add files via upload

---
+++ Cavanagh Portfolio/js/my_style.js
@@ -0,0 +1,10 @@
+function clickOnNews(){
+  var clickbutton = document.getElementById("newsletter")
+  if newslettter.innterHTML = clicked
+}
+var email = prompt("Please enter your e-mail", "Example@example.com");
+
+if (email != clickbutton) {
+    document.getElementById("newsletter").innerHTML =
+    "HThank you for your submission!";
+}
